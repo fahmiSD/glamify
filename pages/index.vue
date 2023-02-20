646cf4441d79b6fe1c3dced787a09df66645d49a
Ata <agastyo2004@gmail.com>
2023-02-20 10:58:26 +0700

    add layout

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,18 @@
   <div>
     <h2>Home</h2>
     <p>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore necessitatibus voluptates aperiam placeat vitae nam corrupti repellendus quidem? Modi illum accusantium optio at officia mollitia ipsa possimus nobis corporis nisi.
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore
+      necessitatibus voluptates aperiam placeat vitae nam corrupti repellendus
+      quidem? Modi illum accusantium optio at officia mollitia ipsa possimus
+      nobis corporis nisi.
     </p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse explicabo cupiditate voluptate culpa, illum nemo ipsum hic ratione dolorum? Veniam reiciendis eum doloremque neque excepturi adipisci quis nulla non eaque!</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse explicabo
+      cupiditate voluptate culpa, illum nemo ipsum hic ratione dolorum? Veniam
+      reiciendis eum doloremque neque excepturi adipisci quis nulla non eaque!
+    </p>
+
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
   </div>
 </template>
 
