638a81609a939224a331038ec26e87a7f4d777fd
Ata <agastyo2004@gmail.com>
2023-02-20 12:03:49 +0700

    add fix navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,24 +2,26 @@
   <div>
     <header class="shadow-sm bg-[#0C3434]">
       <nav class="container mx-auto h-[79px] flex items-center justify-around">
-        <NuxtLink to="/">glamy beauty</NuxtLink>
+        <NuxtLink to="/"
+          ><p class="text-white font-bold text-xl">glamy beauty</p></NuxtLink
+        >
         <form>
           <div class="flex">
-            <div class="relative w-[350px] h-[50px]">
+            <div class="relative w-[471px] h-[55px]">
               <input
                 type="search"
                 id="search-dropdown"
-                class="block p-2.5 w-full h-[50px] z-20 text-sm text-gray-900 bg-gray-50 rounded-2xl border-gray-50 border-2"
+                class="block p-2.5 w-full h-[55px] z-20 text-sm text-gray-900 bg-gray-50 rounded-2xl border-gray-50 border"
                 placeholder="Search for your product"
                 required
               />
               <button
                 type="submit"
-                class="absolute top-0 right-0 bottom-0 p-2.5 text-sm font-medium text-white bg-[#164D4D] rounded-r-2xl border border-white hover:bg-[#164D4D]"
+                class="absolute top-0 right-0 bottom-0 p-2.5 w-[60px] text-sm font-medium text-white bg-[#164D4D] rounded-r-2xl border border-white hover:bg-[#164D4D]"
               >
                 <svg
                   aria-hidden="true"
-                  class="w-5 h-5"
+                  class="w-5 h-5 ml-2"
                   fill="none"
                   stroke="currentColor"
                   viewBox="0 0 24 24"
@@ -37,15 +39,28 @@
             </div>
           </div>
         </form>
-        <ul class="flex gap-4">
+        <ul class="flex gap-24">
           <li>
-            <NuxtLink to="/">Recomendation Request</NuxtLink>
+            <NuxtLink to="/"
+              ><p class="text-white text-center">
+                Recomendation <br />Request
+              </p></NuxtLink
+            >
           </li>
           <li>
-            <NuxtLink to="/about">Review Product</NuxtLink>
+            <NuxtLink to="/about"
+              ><p class="text-white text-center">
+                Review <br />
+                Product
+              </p></NuxtLink
+            >
           </li>
           <li>
-            <NuxtLink to="/products" class="btn">Login / Sign up</NuxtLink>
+            <NuxtLink to="/products" class="btn"
+              ><p class="text-white text-center">
+                Login / <br />Sign up
+              </p></NuxtLink
+            >
           </li>
         </ul>
       </nav>
@@ -59,8 +74,4 @@
   </div>
 </template>
 
-<style scoped>
-.router-link-exact-active {
-  color: #12b488;
-}
-</style>
+<style scoped></style>
