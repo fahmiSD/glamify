22b8d9d7c2b6d1851e4d27d6512803624be9eff2
Fahmi Sabila Dinnulhaq <fahmisabiladinnulhaq@gmail.com>
2023-02-21 15:28:55 +0700

    on hover review

--- pages/detail-product.vue
+++ pages/detail-product.vue
@@ -2,23 +2,17 @@
   <div class="py-16 px-32">
     <div class="grid grid-cols-12 gap-8">
       <div class="col-span-4">
-        <div class="bg-white h-[256px] w-full rounded-3xl p-5 mb-8">
+        <div class="bg-white h-[256px] w-full rounded-3xl p-5 mb-8 group">
           <div class="flex justify-end">
             <div class="absolute">
-              <div
-                class="w-[43px] h-[33px] border border-[#164d4d] rounded-xl flex items-center justify-center drop-shadow-lg"
-              >
+              <div class="w-[43px] h-[33px] border border-[#164d4d] rounded-xl flex items-center justify-center drop-shadow-lg">
                 <img src="/like.svg" alt="" />
               </div>
             </div>
           </div>
           <div class="flex justify-center">
             <div class="absolute mt-16">
-              <button
-                class="text-[#fbf5e3] font-semibold bg-[#164d4d] border border-[#fbf5e3] rounded-xl px-10 py-3"
-              >
-                REVIEW
-              </button>
+              <button class="text-[#fbf5e3] hidden group-hover:block font-semibold bg-[#164d4d] border border-[#fbf5e3] rounded-xl px-10 py-3">REVIEW</button>
             </div>
           </div>
           <div class="flex justify-center mb-1">
@@ -32,60 +26,34 @@
             <img src="/halfYellowStar.svg" alt="star" width="10px" />
             <p class="text-[#797878]">(5)</p>
           </div>
-          <p class="font-semibold text-center text-[#164d4d] mb-1">
-            Retinol in squalane 0.2%
-          </p>
+          <p class="font-semibold text-center text-[#164d4d] mb-1">Retinol in squalane 0.2%</p>
           <p class="font-semibold text-center text-[#797878]">The ordinary</p>
         </div>
         <div class="bg-white w-full h-[166px] rounded-2xl mb-8 p-3">
-          <p class="text-[#164d4d] text-center font-inter font-semibold">
-            Description
-          </p>
+          <p class="text-[#164d4d] text-center font-inter font-semibold">Description</p>
           <p class="text-xs font-poppins text-center text-[#737373]">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur... Read more
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur... Read more
           </p>
         </div>
         <div class="bg-white w-full h-[105px] rounded-2xl p-3 mb-8">
-          <p class="text-[#164d4d] text-center font-inter font-semibold">
-            Ingredients
-          </p>
-          <p class="text-xs font-poppins text-center text-[#737373]">
-            Lorem, ipsum dolor, sit amet, consectetur adipiscing elit, sed do
-            eiusmod, tempor incididunt ut labore et dolore. Read more
-          </p>
+          <p class="text-[#164d4d] text-center font-inter font-semibold">Ingredients</p>
+          <p class="text-xs font-poppins text-center text-[#737373]">Lorem, ipsum dolor, sit amet, consectetur adipiscing elit, sed do eiusmod, tempor incididunt ut labore et dolore. Read more</p>
         </div>
         <div class="bg-white w-full h-[83px] rounded-2xl p-3 mb-8">
-          <p class="text-[#164d4d] text-center font-inter font-semibold">
-            Target
-          </p>
-          <p class="text-xs font-poppins text-center text-[#737373]">
-            Skin, aging, lines
-          </p>
+          <p class="text-[#164d4d] text-center font-inter font-semibold">Target</p>
+          <p class="text-xs font-poppins text-center text-[#737373]">Skin, aging, lines</p>
         </div>
         <div class="bg-white w-full h-[121px] rounded-2xl p-3 mb-8">
-          <p class="text-[#164d4d] text-center font-inter font-semibold">
-            Don'tmix with
-          </p>
-          <p class="text-xs font-poppins text-center text-[#737373]">
-            Lorem, ipsum dolor, sit amet, consectetur adipiscing elit, sed do
-            eiusmod, tempor incididunt ut labore et dolore. Read more
-          </p>
+          <p class="text-[#164d4d] text-center font-inter font-semibold">Don'tmix with</p>
+          <p class="text-xs font-poppins text-center text-[#737373]">Lorem, ipsum dolor, sit amet, consectetur adipiscing elit, sed do eiusmod, tempor incididunt ut labore et dolore. Read more</p>
         </div>
       </div>
       <div class="col-span-8">
         <div class="bg-white w-full h-[143px] rounded-xl mb-8 p-3">
-          <p class="text-[#164d4d] text-center font-inter font-semibold">
-            Top review
-          </p>
+          <p class="text-[#164d4d] text-center font-inter font-semibold">Top review</p>
           <p class="text-xs font-poppins text-center text-[#737373] mb-6">
-            “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud...” Read more
+            “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud...” Read more
           </p>
           <div class="flex justify-between">
             <div class="flex gap-2">
@@ -106,11 +74,7 @@
         </div>
         <div class="grid grid-cols-2 gap-8 mb-5">
           <div class="bg-white w-full h-[153px] rounded-xl p-5">
-            <p class="text-xs font-poppins text-center text-[#737373] mb-8">
-              “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua...”
-              Read more
-            </p>
+            <p class="text-xs font-poppins text-center text-[#737373] mb-8">“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...” Read more</p>
             <div class="flex justify-between">
               <div class="flex gap-2">
                 <img src="/merry.svg" alt="merry" width="31px" />
@@ -129,11 +93,7 @@
             </div>
           </div>
           <div class="bg-white w-full h-[153px] rounded-xl p-5">
-            <p class="text-xs font-poppins text-center text-[#737373] mb-8">
-              “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua...”
-              Read more
-            </p>
+            <p class="text-xs font-poppins text-center text-[#737373] mb-8">“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...” Read more</p>
             <div class="flex justify-between">
               <div class="flex gap-2">
                 <img src="/merry.svg" alt="merry" width="31px" />
@@ -152,11 +112,7 @@
             </div>
           </div>
           <div class="bg-white w-full h-[153px] rounded-xl p-5">
-            <p class="text-xs font-poppins text-center text-[#737373] mb-8">
-              “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua...”
-              Read more
-            </p>
+            <p class="text-xs font-poppins text-center text-[#737373] mb-8">“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...” Read more</p>
             <div class="flex justify-between">
               <div class="flex gap-2">
                 <img src="/merry.svg" alt="merry" width="31px" />
@@ -175,11 +131,7 @@
             </div>
           </div>
           <div class="bg-white w-full h-[153px] rounded-xl p-5">
-            <p class="text-xs font-poppins text-center text-[#737373] mb-8">
-              “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua...”
-              Read more
-            </p>
+            <p class="text-xs font-poppins text-center text-[#737373] mb-8">“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...” Read more</p>
             <div class="flex justify-between">
               <div class="flex gap-2">
                 <img src="/merry.svg" alt="merry" width="31px" />
@@ -199,11 +151,7 @@
           </div>
         </div>
         <div class="flex justify-center items-center mb-10">
-          <button
-            class="bg-[#fbf5e3] rounded-lg px-6 py-2 text-[#164d4d] font-bold"
-          >
-            SHOW ALL REVIEW
-          </button>
+          <button class="bg-[#fbf5e3] rounded-lg px-6 py-2 text-[#164d4d] font-bold">SHOW ALL REVIEW</button>
         </div>
         <div class="flex justify-center mb-8">
           <p class="text-[#fbf5e3] font-poppins text-xl">Featured in</p>
@@ -214,17 +162,9 @@
               <img src="/lighting.svg" alt="lighting" />
               <img src="/brush.svg" alt="brush" />
             </div>
-            <p class="text-center mb-5">
-              “I have a very formal event tomorrow and need help finding a long
-              stay red lipstick! I won’t have time to retouch it for hours and
-              don’t want to look messy in the picures. Can anyone help?”
-            </p>
+            <p class="text-center mb-5">“I have a very formal event tomorrow and need help finding a long stay red lipstick! I won’t have time to retouch it for hours and don’t want to look messy in the picures. Can anyone help?”</p>
             <div class="flex justify-center">
-              <button
-                class="bg-[#164d4d] text-[#ffffff] rounded-xl text-sm px-2 py-1 font-bold"
-              >
-                RECOMMEND NOW
-              </button>
+              <button class="bg-[#164d4d] text-[#ffffff] rounded-xl text-sm px-2 py-1 font-bold">RECOMMEND NOW</button>
             </div>
           </div>
           <div class="bg-white h-[245px] w-full rounded-3xl p-5 mb-4">
@@ -232,17 +172,9 @@
               <img src="/lighting.svg" alt="lighting" />
               <img src="/brush.svg" alt="brush" />
             </div>
-            <p class="text-center mb-5">
-              “I have a very formal event tomorrow and need help finding a long
-              stay red lipstick! I won’t have time to retouch it for hours and
-              don’t want to look messy in the picures. Can anyone help?”
-            </p>
+            <p class="text-center mb-5">“I have a very formal event tomorrow and need help finding a long stay red lipstick! I won’t have time to retouch it for hours and don’t want to look messy in the picures. Can anyone help?”</p>
             <div class="flex justify-center">
-              <button
-                class="bg-[#164d4d] text-[#ffffff] rounded-xl text-sm px-2 py-1 font-bold"
-              >
-                RECOMMEND NOW
-              </button>
+              <button class="bg-[#164d4d] text-[#ffffff] rounded-xl text-sm px-2 py-1 font-bold">RECOMMEND NOW</button>
             </div>
           </div>
         </div>
